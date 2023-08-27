187d6f9a168b0cfbc7b189c2b2ba01bfcf67bd57
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-08-27 15:30:34 -0400

    ref: removes config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,9 @@ import { AppController } from './app.controller';
 import { MediasModule } from './medias/medias.module';
 import { PostsModule } from './posts/posts.module';
 import { PublicationsModule } from './publications/publications.module';
-import { ConfigModule } from '@nestjs/config';
-
-const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: ENV ? `.env.${ENV}` : '.env',
-    }),
-    MediasModule,
-    PostsModule,
-    PublicationsModule,
-  ],
+  imports: [MediasModule, PostsModule, PublicationsModule],
   controllers: [AppController],
 })
 export class AppModule {}
