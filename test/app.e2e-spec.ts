0f555ee258a6f3426a57d8982bf71177bd2650c9
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-08-27 15:29:59 -0400

    chore: path mapping config

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,6 +5,7 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  console.log(process.env.DATABASE_URL);
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
